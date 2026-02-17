fa509471cdc65b96e9666ed599bb5bb741edcf5c
Mohamedzahangir <mohamedzahangiralimolla@gmail.com>
2026-02-17 18:35:54 +0530

    Enhanced shedular with error handling , execution logging,and job listeners

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from modules.scheduler import start_scheduler
 
 
 if __name__ == "__main__":
-    topic = "importance of non vibe coders in the tech industry"
+    topic = "Impact of AI on Society"
     start_scheduler(topic)
--- modules/scheduler.py
+++ modules/scheduler.py
@@ -1,12 +1,35 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.events import EVENT_JOB_ERROR, EVENT_JOB_EXECUTED
 from modules.workflow import run_workflow
+import traceback
+
+
+def job_listener(event):
+    if event.exception:
+        print("\n❌ Job crashed!")
+        print(traceback.format_exc())
+    else:
+        print("✅ Job executed successfully.")
 
 
 def start_scheduler(topic: str):
     scheduler = BlockingScheduler()
 
+    def scheduled_job():
+        try:
+            result = run_workflow(topic)
+            if result:
+                print("\nPost generated and saved successfully.\n")
+            else:
+                print("\nDuplicate detected. Skipped generation.\n")
+        except Exception as e:
+            print("\n🚨 Unexpected error in scheduled job:")
+            print(str(e))
+
+    scheduler.add_listener(job_listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
+
     scheduler.add_job(
-        lambda: run_workflow(topic),
+        scheduled_job,
         trigger="interval",
         minutes=1
     )
